b1dcd59362ad62391e350ed433f372e030d80c9e
jazper298 <minatosjutsu@gmail.com>
2020-02-20 23:57:32 +0800

    add splash screen and loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,47 +4,25 @@ import 'package:flutter_instagram_clone/models/user_data.dart';
 import 'package:flutter_instagram_clone/screens/feed_screen.dart';
 import 'package:flutter_instagram_clone/screens/home_screen.dart';
 import 'package:flutter_instagram_clone/screens/login_screen.dart';
-import 'package:flutter_instagram_clone/screens/signup_screen.dart';
+import 'package:flutter_instagram_clone/screens/main2.dart';
+import 'package:flutter_instagram_clone/screens/onboarding_screen.dart';
+import 'package:flutter_instagram_clone/screens/splash_screen.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  Widget _getScreenId(){
-    return StreamBuilder<FirebaseUser>(
-      stream: FirebaseAuth.instance.onAuthStateChanged,
-      builder: (BuildContext context, snapshot) {
-        if (snapshot.hasData){
-          Provider.of<UserData> (context).currentUserId = snapshot.data.uid;
-          return HomeScreen();
-        }else {
-          return LoginScreen();
-        }
-      },
-    );
-  }
+var routes = <String, WidgetBuilder>{
+  "/home": (BuildContext context) => HomeScreen(),
+  "/onboard": (BuildContext context) => OnboardingScreen(),
+  "/myapp2": (BuildContext context) => MyApp2(),
+  "/login": (BuildContext context) => LoginScreen(),
+};
 
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => UserData(),
-      child: MaterialApp(
-        title: 'Instagram Clone',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryIconTheme: Theme.of(context).primaryIconTheme.copyWith(
-            color: Colors.black,
-          ),
-          // primarySwatch: Colors.blue,
-        ),
-        home: _getScreenId(),
-        routes: {
-          LoginScreen.id: (context) => LoginScreen(),
-          SignupScreen.id: (context) => SignupScreen(),
-          FeedScreen.id: (context) => FeedScreen(),
-        },
-      ),
-    );
-  }
-}
+void main() => runApp(new MaterialApp(
+    theme:
+    ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
+    routes: routes)
+);
+
+
